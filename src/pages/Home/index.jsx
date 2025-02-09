c623f7a20067e25e076400e75ffaa0afdca4811b
Thiago Monteiro <79913395+Thiagooms@users.noreply.github.com>
2025-02-09 15:27:52 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom"
-import { Header } from "../../components/Header"
+import { Header } from "components/Header"
 import Slider from "react-slick";
-import styles from "./Home.module.css";
-import Banner01 from "../../assets/imgs/slider1.png";
-import Banner02 from "../../assets/imgs/slider2.png";
-import { GenericSection } from "../../components/GenericSection";
-import ProductsJSON from "../../mocks/products.json";
+import styles from "Home.module.css";
+import Banner01 from "assets/imgs/slider1.png";
+import Banner02 from "assets/imgs/slider2.png";
+import { GenericSection } from "components/GenericSection";
+import ProductsJSON from "mocks/products.json";
 
 export function Home(){
     
