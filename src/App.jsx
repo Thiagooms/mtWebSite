2d1e08d93e16ebcf51de816bdd88a814426f7e4c
Thiago Monteiro <79913395+Thiagooms@users.noreply.github.com>
2025-02-09 15:27:05 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { Home } from "./pages/Home";
-import { Product } from "./pages/Product";
+import { Home } from "pages/Home";
+import { Product } from "pages/Product";
 
 const router = createBrowserRouter([
   {
